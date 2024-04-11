94efb7e8d33f82eca0d7001ddaa399448291ed8c
Mobi Boi <149164613+AbedElRahmanAlSharif@users.noreply.github.com>
2024-04-11 18:56:52 +0300

    Add files via upload
    This is an todo app, but still under construction

---
+++ Signin.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+import 'package:todo_me/login.dart';
+import 'homescreen.dart';
+class Signin extends StatefulWidget {
+  const Signin({super.key});
+
+  @override
+  State<Signin> createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  final __formKey = GlobalKey<FormState>();
+
+  final  _password = TextEditingController();
+  final  _cpassword = TextEditingController();
+  final  _username = TextEditingController();
+  final  _email = TextEditingController();
+  RegExp regex = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+
+  @override
+  Widget build(BuildContext context) {
+  void _register(){
+    if(__formKey.currentState!.validate()){
+      //! Save the form
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const  home_screen()
+      ),
+      );
+    }
+  }
+    return Scaffold(
+
+      backgroundColor: const Color(0xEEEEEEEE),
+      body: 
+      Form(
+        key: __formKey,
+        
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Transform(
+                transform: Matrix4.translationValues(-100, -120, 0),
+                child: Container(
+                  width: 300,
+                  height: 233,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromRGBO(156, 229, 224, 50),
+                  ),
+                ),
+              ),
+              Transform(
+                transform: Matrix4.translationValues(-250, -280, 0),
+                child: Container(
+                  width: 300,
+                  height: 233,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromRGBO(156, 229, 224, 50),
+                  ),
+                ),
+              ), 
+              const Center(
+                child:  Text(
+            'Welcome Onboard',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+              ),
+              const Center(
+                 child:  Text(
+            'Help us to help you',
+              ),
+              ),
+             const SizedBox(height: 5),
+              Center(
+                child: Container(
+                   width: 310,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: TextFormField(
+                    controller: _username,
+                    decoration: 
+                    const InputDecoration(
+                      labelText: 'UserName',
+                      hintText: 'Enter your username',
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(20.0),
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) => _username.text = value!,
+                  ),
+                ),
+              ),
+             const  SizedBox(height: 15),
+              Container(
+                 width: 310,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                child: TextFormField(
+                  controller: _email,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Enter your email',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.all(20.0),
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    if(value.contains('@gmail.com') == false){
+                      return 'Your email should be ends with @gmail.com';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _email.text = value!,
+                ),
+              ),
+             const  SizedBox( height: 15),
+              Container(
+                 width: 310,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                child: TextFormField(
+                  controller: _password,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.all(20.0),
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    if (!regex.hasMatch(value)) {
+                      return 'Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _password.text = value!,
+                ),
+              ),
+             const SizedBox(height: 15,),
+              Container(
+                width: 310,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                child: TextFormField(
+                  controller: _cpassword,
+                  decoration: const InputDecoration(
+                    labelText: 'Confirm Password',
+                    hintText: 'Enter your password again',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.all(20.0),
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    if (value != _password.text) {
+                      return 'Password does not match';
+                    }
+                    else if(_cpassword == _password){
+                      return 'Password does not match';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _cpassword.text = value!,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: SizedBox( 
+                  width: 310,
+                  height: 60,
+                  child: ElevatedButton(
+                  onPressed: _register,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(58, 190, 208, 75),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text('Register' , style:  TextStyle(color:  Colors.white),),
+                ),
+              ),
+              ),
+                         const SizedBox(height: 15,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Already have an account?',
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) =>  login_Page()),
+                      );
+                    },
+                    child: const Text('Log in'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ homescreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class home_screen extends StatefulWidget {
+  const home_screen({super.key});
+
+  @override
+  State<home_screen> createState() => _homescreenState();
+}
+
+class _homescreenState extends State<home_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
